ed63cb86ebb443936b0bbeb020cfb5454aeff2c7
kanithahomjun <36620709+kanithahomjun@users.noreply.github.com>
2021-02-28 20:04:27 +0700

    HW_17
    HW_17

---
+++ 17/app/productitem/productitem.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core' ;
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-productitem',
+  templateUrl: './productitem.component.html',
+  styleUrls: ['./productitem.component.css']
+})
+export class ProductitemComponent implements OnInit {
+  @Input()
+  product: Product;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ 17/app/productlist/productlist.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-productlist',
+  templateUrl: './productlist.component.html',
+  styleUrls: ['./productlist.component.css']
+  })
+  export class ProductListComponent implements OnInit {
+  products: Product[];
+    constructor() {
+      this.products = [];
+      this.products.push({
+        name: 'ส้มโอ',
+        price: 990
+      });
+      this.products.push({
+        name: 'แตงโม',
+        price: 1000
+      });
+      this.products.push({
+        name: 'มะพร้าวนํ้าหอม',
+        price: 500
+      });
+    }
+  ngOnInit() {}
+  }
