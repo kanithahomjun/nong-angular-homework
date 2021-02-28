cc8c68b5150a3cb79a84a701371d5b5685ee1a42
kanithahomjun <36620709+kanithahomjun@users.noreply.github.com>
2021-02-28 17:53:08 +0700

    HW_13.2
    HW_13.2

---
+++ 13.2/todolist/todolist.component.ts
@@ -0,0 +1,20 @@
+import { NodeWithI18n } from '@angular/compiler';
+import { Component,Input } from '@angular/core';
+@Component({
+selector: 'app-todolist',
+templateUrl: './todolist.component.html',
+styleUrls: ['./todolist.component.css']
+})
+export class ToDoListComponent {
+    
+    @Input()
+    title: string;
+    items = ["Coffee", "Breakfast"];
+
+    constructor() {}
+    AddItems() {
+       
+    }
+       
+
+}
